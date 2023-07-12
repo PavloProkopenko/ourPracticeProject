64656f9bf6b776f5808a01ed820871c2cb69385c
Pavlo Prokopenko <stpasha6@gmail.com>
2023-07-12 16:14:45 +0300

    We started making our program

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
+    <Provider>
+      
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+
+    </Provider>
   );
 }
 
