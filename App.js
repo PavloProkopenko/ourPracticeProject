5289e1abc657339156db9f629376e40d33ff76ea
Pavlo Prokopenko <stpasha6@gmail.com>
2023-07-13 18:15:28 +0300

    Added function which change screens.
    Було додано багато бібліотке (react-native-navigation,
    react-native-gesture-handler тощо) для створення функції зміни екранів.
    Тепер, натиснувши на кнопку "Get a ride" ми можемо перейти на наступний
    екран.
    
    Для цього використано принцип стеку. По суті, екрани накладаються один
    на одного, що надає змогу швидко повернутися до головного екрану
    застосунку.
    
    Далі, буде додано мапу.

--- App.js
+++ App.js
@@ -4,14 +4,39 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      
-    <View style={styles.container}>
-       <HomeScreen />
-    </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+            <Stack.Navigator>
+              <Stack.Screen 
+                name = 'HomeScreen'
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen 
+                name = 'MapScreen'
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    
+    
 
     </Provider>
   );
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from '@react-navigation/native';
 
 
 const data = [
@@ -20,19 +21,23 @@ const data = [
 ];
 
 const NavOptions = () => {
+	const navigation = useNavigation();
+
 	return (
 		<FlatList
 			data={data}
 			horizontal
 			keyExtractor={(item) => item.id}
 			renderItem={({ item }) => (
-				<TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} >
+				<TouchableOpacity
+				onPress={() => navigation.navigate(item.screen)}
+				style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`} >
 					<View>
 						<Image
 							style={{ width: 120, height: 120, resizeMode: "contain" }}
 							source={{ uri: item.image }}
 						/>
-						<Text style={tw`-mt-2 text-lg font-semibold`}>{item.title}</Text>
+						<Text style={tw`-mt-1 text-lg font-semibold`}>{item.title}</Text>
 						<Icon
 							style={tw`p-2 bg-black rounded-full w-10 mt-4`}
 							type="antdesign" color="white" name="arrowright" />
