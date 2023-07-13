0e859b8abd7aba1fafb226e570251ed4d05fea09
Pavlo Prokopenko <stpasha6@gmail.com>
2023-07-13 14:21:59 +0300

    Added tailwindCSS classnames

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
     <Provider store={store}>
       
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> Slavik MALADEC </Text>
       <StatusBar style="auto" />
     </View>
 
